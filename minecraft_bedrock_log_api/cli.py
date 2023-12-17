910e9d5be71c43cd9c08f143e3db396a87d84555
aoirint <aoirint@gmail.com>
2023-12-17 15:51:46 +0900

    impl api

--- minecraft_bedrock_log_api/cli.py
+++ minecraft_bedrock_log_api/cli.py
@@ -1,2 +1,93 @@
+import os
+from argparse import ArgumentParser
+from typing import Annotated, Any
+
+import jwt
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+
+def create_app(
+    jwt_secret_key: str,
+) -> FastAPI:
+    app = FastAPI()
+
+    def get_authenticated_user(
+        authorization: Annotated[HTTPAuthorizationCredentials, Depends(HTTPBearer)],
+    ) -> str:
+        token = authorization.credentials
+        credential_exception = HTTPException(
+            status=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+        username: str | None = None
+        try:
+            payload: dict[str, Any] = jwt.decode(
+                jwt=token,
+                key=jwt_secret_key,
+                algorithms=["HS256"],
+            )
+
+            username = payload.get("sub")
+        except jwt.PyJWTError:
+            raise credential_exception
+
+        if username is None:
+            raise credential_exception
+
+        return username
+
+    @app.post("/api")
+    def post_api(
+        log: str,
+        current_user: Annotated[str, Depends(get_authenticated_user)],
+    ):
+        """
+        Fluentdのhttp outputからのログ出力を受け取る
+        """
+        print(log)
+
+    return app
+
+
 def main() -> None:
-    pass
+    load_dotenv()
+
+    default_jwt_secret_key: str | None = os.environ.get("APP_JWT_SECRET_KEY") or None
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--host",
+        type=str,
+        default="0.0.0.0",
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=8000,
+    )
+    parser.add_argument(
+        "--jwt_secret_key",
+        type=str,
+        default=default_jwt_secret_key,
+        required=default_jwt_secret_key is None,
+    )
+    args = parser.parse_args()
+
+    host: str = args.host
+    port: int = args.port
+    jwt_secret_key: str = args.jwt_secret_key
+
+    app = create_app(
+        jwt_secret_key=jwt_secret_key,
+    )
+
+    uvicorn.run(
+        app=app,
+        host=host,
+        port=port,
+    )
